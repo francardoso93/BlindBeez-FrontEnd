872875a3375bede3c9daf540c05d6133136668d8
Francisco Cardoso <francardoso@outlook.com>
2019-10-22 21:44:07 -0200

    Header ajustado com CSS limpo e sem utilizacao de jquery

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
-import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, HeaderMenuComponent],
+  declarations: [HeaderComponent],
   imports: [
     CommonModule
   ],
