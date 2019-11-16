42f33718180a826114701954fda096033d6b8f7c
Francisco Ferreira Cardoso <francisco.cardoso@totvs.com.br>
2019-11-16 17:30:28 -0300

    Ajustando links e formulario de agendamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SchedulerComponent } from './scheduler/scheduler.component';
 
@@ -8,8 +8,14 @@ const routes: Routes = [
   { path: 'agendamento', component: SchedulerComponent }
 ];
 
+const routerOptions: ExtraOptions = {
+  useHash: false,
+  anchorScrolling: 'enabled',
+  // ...any other options you'd like to use
+};
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-scheduler',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduler.component.css']
 })
 export class SchedulerComponent implements OnInit {
+  clientScheduleForm: FormGroup;
+  showMessage: boolean;
+  submitted: boolean;
 
-  constructor() { }
+  public datemask = [
+    /[0-9]/,
+    /[0-9]/,
+    "/",
+    /[0-1]/,
+    /[0-9]/,
+    "/",
+    /[0-9]/,
+    /[0-9]/,
+    /[0-9]/,
+    /[0-9]/
+  ];
+
+  constructor(public fb: FormBuilder, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.showMessage = false;
+    this.submitted = false;
+
+    this.clientScheduleForm = this.fb.group({
+      name: [null, Validators.required],
+      email: [null, Validators.compose([
+          Validators.required,
+          Validators.pattern(/\b\S+@\S+\b/g),
+        ])],
+      date: [null, Validators.compose([
+        Validators.required,
+        Validators.pattern('^(0[1-9]|[12][0-9]|3[01])[/](0[1-9]|1[012])[/][0-2][0-9][0-9][0-9]'),
+      ])],
+    });
   }
 
+  submitForm(value: any) {
+    console.log(value);
+
+    this.submitted = true;
+
+    if (this.clientScheduleForm.valid) {
+      this.showMessage = true;
+      //TODO: Submit real do formulario JSON
+      setTimeout(() => {
+        const messageHeading = <HTMLElement>document.querySelector('#message-heading');
+        messageHeading.focus();
+      }, 100);
+    } else {
+      setTimeout(() => {
+        const errorHeading = <HTMLElement>document.querySelector('#error-heading');
+        errorHeading.focus();
+      }, 100);
+    }
+  }
 }
