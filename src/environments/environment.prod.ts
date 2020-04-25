ba48767219a82c355563245b19dd18152fddf684
Francisco F. Cardoso <francardoso@outlook.com>
2020-04-25 14:00:37 -0300

    Mudando coisas de ordem + removendo '/' final do environment de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiroot: 'https://wiv03pbxu4.execute-api.us-east-1.amazonaws.com/dev/',
+  apiroot: 'https://wiv03pbxu4.execute-api.us-east-1.amazonaws.com/dev',
   companies: '/companies',
   schedules: '/schedules',
   client_schedule: '/client-scheduler',
