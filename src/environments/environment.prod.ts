9a2ba7e28340a2d17d54a3685a8d8620a68da2db
Francisco F. Cardoso <francardoso@outlook.com>
2020-04-25 09:14:17 -0300

    Apontando para backend AWS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiroot: 'https://wiv03pbxu4.execute-api.us-east-1.amazonaws.com/dev/',
+  companies: '/companies',
+  schedules: '/schedules',
+  client_schedule: '/client-scheduler',
 };
