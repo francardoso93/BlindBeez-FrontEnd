a1a9b7e848613db3e3bcea0ca003ec3a2e30e841
Francisco F. Cardoso <francardoso@outlook.com>
2020-04-29 22:09:59 -0300

    Apontando para backend sem problema do cors

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiroot: 'https://063dhz09hl.execute-api.us-east-1.amazonaws.com/dev',
+  apiroot: 'https://0a4uuwwvc6.execute-api.us-east-1.amazonaws.com/dev',
   companies: '/companies',
   schedules: '/schedules',
   client_schedule: '/client-scheduler',
