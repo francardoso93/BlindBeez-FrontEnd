d41e714614868ad52823015578a7c4e96a55be88
Francisco F. Cardoso <francardoso@outlook.com>
2020-05-09 17:25:14 -0300

    Trocando url do backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiroot: 'https://v2p2cdtpsf.execute-api.us-east-1.amazonaws.com/aws',
+  apiroot: 'https://t4rtgby31f.execute-api.us-east-1.amazonaws.com/aws',
   companies: '/companies',
   schedules: '/schedules',
   client_schedule: '/client-scheduler',
