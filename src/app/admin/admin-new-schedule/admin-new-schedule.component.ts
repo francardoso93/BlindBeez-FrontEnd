d11e3ac3e8228d950cb68396e4ee67ff8be2cecc
Francisco Cardoso <francardoso@outlook.com>
2020-06-06 14:26:29 -0300

    Mascara e regex de data em environment

--- src/app/admin/admin-new-schedule/admin-new-schedule.component.ts
+++ src/app/admin/admin-new-schedule/admin-new-schedule.component.ts
@@ -4,7 +4,7 @@ import { Observable, fromEventPattern } from 'rxjs';
 import { Router } from '@angular/router';
 import { Company } from 'src/app/scheduler/company';
 import { SchedulerService } from 'src/app/scheduler/scheduler.service';
-import { SubmitResultService } from 'src/app/scheduler/submit-result/submit-result.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-admin-new-schedule',
@@ -18,16 +18,35 @@ export class AdminNewScheduleComponent implements OnInit {
   companyId: number;
   availableCompanies: Observable<Company[]>;
 
+  dateReg: RegExp = environment.dateReg;
+  dateMask = environment.dateMask;
+
   constructor(
     public fb: FormBuilder,
     private schedulerService: SchedulerService,
-    private submitResultService: SubmitResultService,
     private router: Router, ) { }
 
   ngOnInit() {
     this.getCompanies();
     this.newScheduleForm = this.fb.group({
       companyId: ['', Validators.required],
+      initialDate: [null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            this.dateReg
+          )
+        ])],
+      finalDate: [null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(
+            this.dateReg
+          )
+        ])],
+      initalTime: ['', Validators.required], //TODO: permite de 00:00 a 23:59 (Mascara + validação)
+      endTime: ['', Validators.required], //TODO: permite de 00:00 a 23:59 (Mascara + validação)
+      minuteInterval: ['', Validators.required], //TODO: Permite apenas de 1 a 60 (Mascara + validação)
     });
   }
 
@@ -36,6 +55,6 @@ export class AdminNewScheduleComponent implements OnInit {
   }
 
   async submitForm(newScheduleForm: any) {
-    console.log("submitted!");
+    console.log(newScheduleForm);
   };
 }
--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router';
 import { SchedulerService } from './scheduler.service';
 import { SubmitResultService } from './submit-result/submit-result.service';
 import { ClientScheduler } from './client.scheduler';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-scheduler',
@@ -25,20 +26,8 @@ export class SchedulerComponent implements OnInit {
   availableCompanies: Observable<Company[]>;
   times: Schedule[];
 
-  dateReg: RegExp = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-  
-  public datemask = [
-    /[0-3]/,
-    /[0-9]/,
-    '/',
-    /[0-1]/,
-    /[0-9]/,
-    '/',
-    /[1-2]/,
-    /[0-9]/,
-    /[0-9]/,
-    /[0-9]/
-  ];
+  dateReg: RegExp = environment.dateReg;  
+  dateMask = environment.dateMask;
 
 
   constructor(
