7e0b0d6a77023b70c710cc2a9fc664d668cfc801
Francisco Cardoso <francardoso@outlook.com>
2020-07-08 22:34:24 -0300

    Facilitando reaproveitamento da funcao refresh schedule list

--- src/app/admin/admin-schedule-viewer/calendar-table/calendar-table.service.ts
+++ src/app/admin/admin-schedule-viewer/calendar-table/calendar-table.service.ts
@@ -1,27 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Schedule } from 'src/app/scheduler/schedule';
-import { environment } from 'src/environments/environment';
-import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Schedule } from "src/app/scheduler/schedule";
+import { environment } from "src/environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CalendarTableService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   listSchedules(date: string, companyId: number): Observable<Schedule[]> {
     const initialDate: string = date + "T00:00:00";
     const finalDate: string = date + "T23:59:59";
     return this.http.get<Schedule[]>(
-      environment.apiroot + environment.schedules + "?initialDate=" + initialDate  + "&finalDate=" + finalDate + "&companyId=" + companyId,
+      environment.apiroot +
+        environment.schedules +
+        "?initialDate=" +
+        initialDate +
+        "&finalDate=" +
+        finalDate +
+        "&companyId=" +
+        companyId
     );
   }
 
   deleteSchedule(scheduleId: string, callback: Function) {
     const url = environment.apiroot + environment.schedules + "/" + scheduleId;
     console.log(url);
-    this.http.delete(environment.apiroot + environment.schedules + "/" + scheduleId).subscribe(() => setTimeout(callback(), environment.refreshFormAfterDeleteTimeout));;
+    this.http
+      .delete(environment.apiroot + environment.schedules + "/" + scheduleId)
+      .subscribe(() => this.refreshScheduleList(callback));
+  }
+
+  refreshScheduleList(callback: Function) {
+    setTimeout(callback(), environment.refreshTableTimeout);
   }
 }
