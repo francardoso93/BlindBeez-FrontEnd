517b515f26e2fe8004739d962adc18567a4b9bbb
Francisco Cardoso <francardoso@outlook.com>
2020-07-11 12:10:06 -0300

    Apresentou um erro no heading order referente ao footer (no app.compenet.spec.ts mesmo)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import * as axe from 'axe-core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeModule } from './home/home.module';
+import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { AdminModule } from './admin/admin.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HomeModule,
+        HeaderModule,
+        FooterModule,
+        SchedulerModule,
+        AdminModule,
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +33,22 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scheduler-blind-beez'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('scheduler-blind-beez');
-  });
-
-  it('should render title in a h1 tag', () => {
+  it('should be no accessibility issues', done => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to scheduler-blind-beez!');
+
+    axe.run(compiled, {}, (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      if (results.violations.length > 0) {
+        console.log(JSON.stringify(results.violations, null, 4));
+      }
+
+      expect(results.violations.length).toBe(0);
+      done();
+    });
   });
 });
